abfbe0ee337091eb91b33afc79b992dcf8bbbef0
lifei128 <lifeibigdata@gmail.com>
2016-10-19 10:25:42 +0800

    fix

---
+++ agent/src/main/java/com/tansuo/agent/Test.java
@@ -0,0 +1,10 @@
+package com.tansuo.agent;
+
+/**
+ * Created by lifei on 16/10/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("----hello----");
+    }
+}
